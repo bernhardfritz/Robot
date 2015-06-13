41fe942000f665204b9310aa1fc4dd32ed31b3f8
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2015-06-13 20:41:22 +0200

    fix

--- untitled folder/Robot/src/backup/RobotCommand.java
+++
@@ -1,7 +0,0 @@
-package backup;
-
-import com.example.robot.Robot;
-
-public interface RobotCommand {
-	String execute(Robot robot) throws InterruptedException;
-}
--- untitled folder/Robot/src/com/example/robot/Command.java
+++
@@ -1,6 +0,0 @@
-package com.example.robot;
-
-public interface Command {
-	String execute(Robot robot) throws InterruptedException;
-	boolean isAborted();
-}
